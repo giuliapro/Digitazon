38d10f07a572e5cd8aa57b93163ae30e64269acf
giuliapro <giulia.orsa@gmail.com>
2023-08-22 16:05:07 +0200

    esercizi js

---
+++ modulo-2/esercizio2.js
@@ -0,0 +1,12 @@
+// creare una nuova variabile che contiene il numero generato da Math.random()
+// stampare a video se il numero è maggiore di 0.5, di 0.8 e minore di 0.2
+
+let numeroCasuale = Math.random()
+
+if (numeroCasuale > 0.5 || numeroCasuale > 0.8) {
+    console.log(numeroCasuale)
+}
+
+if (numeroCasuale < 0.2) {
+    console.log(numeroCasuale)
+}
---
+++ modulo-2/esercizio4.js
@@ -0,0 +1,8 @@
+// creiamo una variabile chiamata numero e le assegnamo 45 come valore
+// poi controlliamo se quel numero è maggiore di 40 e se lo è la dividiamo per 3 stampandone il resto
+
+let numero = 45
+
+if (numero > 40) {
+    console.log(numero%3)
+}
